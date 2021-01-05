3f760ec87c322a123f3a923f649d87b6b2f0946a
japson1987 <182886016@qq.com>
2021-01-05 16:30:18 +0800

    update cdn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
     production: false,
     requestTimeout: 30000,
-    appServerUrl: "https://walletserver.cypherium.io", //APPServer API
+    appServerUrl: "https://walletsvrcdn.cypherium.io", //APPServer API
     cypherium: {
-        provider: 'https://pubnodes.cypherium.io',
+        provider: 'https://pubnodescdn.cypherium.io',
         pledgeContractAddr: '0x0000000000000000000000000000000000000081',
         pledgeContractAbi: null,
         privateKey: ''
